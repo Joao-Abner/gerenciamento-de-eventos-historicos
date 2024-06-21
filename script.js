189598c00a27414a09b83f0707ca204404815dda
Joao-Abner <joaoabnerpereira@gmail.com>
2024-06-21 00:06:37 -0300

    Add menu-left

--- script.js
+++ script.js
@@ -15,3 +15,34 @@ document.addEventListener("DOMContentLoaded", function () {
     searchButton.addEventListener("click", toggleSearch);
   }
 });
+
+// animacao menu esquerdo
+document.addEventListener("DOMContentLoaded", function () {
+  var menuButton = document.querySelector(".menu-button");
+  var menuLateralEsquerdo = document.querySelector(".menu-lateral-esquerdo");
+
+  menuButton.addEventListener("click", function () {
+    if (menuLateralEsquerdo.classList.contains("oculto")) {
+      menuLateralEsquerdo.classList.remove("oculto");
+    } else {
+      menuLateralEsquerdo.classList.add("oculto");
+    }
+  });
+});
+
+//classe ativa ao botao clicado
+document.addEventListener("DOMContentLoaded", function () {
+  const buttons = document.querySelectorAll(
+    ".new-event-button,.star-button,.menu-all-button"
+  );
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+      // Remove a classe active de todos os botões
+      buttons.forEach((btn) => btn.classList.remove("active"));
+
+      // Adiciona a classe active ao botão clicado
+      this.classList.add("active");
+    });
+  });
+});
